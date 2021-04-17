5f7dc3a64c7180dbad2293fb8fc3d72634b39813
Tan Nguyen <tannd1993@gmail.com>
2021-04-17 11:40:24 +0700

    feat: implement react-native-elements

--- src/screens/Wallet/index.tsx
+++ src/screens/Wallet/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
 const Wallet = () => {
   return (
     <View>
       <Text>Wallet screen</Text>
+      <Button title="Hello Button" />
     </View>
   );
 };
