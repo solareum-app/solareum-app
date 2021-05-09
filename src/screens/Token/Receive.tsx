2a94de1fe377ed530e9fd787496dba29c1654ed4
Tam Pham <tampham47@live.com>
2021-05-09 17:51:22 +0700

    Add receive screen

--- src/screens/Token/Receive.tsx
+++ src/screens/Token/Receive.tsx
@@ -1,12 +1,58 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
+
+import { RoundedButton } from '../../components/RoundedButton';
 import { grid, typo } from '../../components/Styles';
+
+const s = StyleSheet.create({
+  main: {
+    marginTop: 20,
+  },
+  qr: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    borderRadius: 4,
+    borderWidth: 16,
+    marginTop: 80,
+    marginBottom: 40,
+    borderColor: 'white'
+  },
+  footer: {
+    marginTop: 40,
+  },
+  control: {
+    flexDirection: 'row',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginBottom: 20,
+  },
+  controlItem: {
+    marginLeft: 12,
+    marginRight: 12,
+  },
+});
 
 export const Receive = () => {
   return (
     <View style={grid.body}>
       <Text style={typo.title}>Nhận SOL</Text>
-      <Text style={typo.normal}>Gởi Veniam ipsum voluptate consequat est in sunt duis sunt laboris esse pariatur. Consectetur pariatur nulla consectetur minim. Quis tempor et et sit labore. Quis velit consequat mollit culpa et.</Text>
+      <View style={s.main}>
+        <View style={s.qr}>
+          <QRCode value="751dKZJazx8BrCkK1wxgLNzQcZBVzJbVgYbi8KKVe7MH" size={220} />
+        </View>
+        <Text style={typo.helper}>Chỉ chuyển Solana (SOL) vào địa chỉ này. Việc chuyển token khác vào địa chỉ này có thể dẫn đến mất toàn toàn các token đó.</Text>
+      </View>
+      <View style={s.footer}>
+        <View style={s.control}>
+          <View style={s.controlItem}>
+            <RoundedButton onClick={() => null} title="Sao chép" iconName="addfile" />
+          </View>
+          <View style={s.controlItem}>
+            <RoundedButton onClick={() => null} title="Chia sẻ" iconName="upload" />
+          </View>
+        </View>
+      </View>
     </View>
   )
 };
