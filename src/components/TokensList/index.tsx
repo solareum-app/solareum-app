c5dfab0885904f27a37bdaeaf17fcd968400f344
Tan Nguyen <tannd1993@gmail.com>
2021-05-15 11:07:55 +0700

    validate url

--- src/components/TokensList/index.tsx
+++ src/components/TokensList/index.tsx
@@ -8,6 +8,30 @@ import { COLORS } from '../../theme/colors';
 import Routes from '../../navigators/Routes';
 import { useTokenInfos } from '../../core/TokenRegistryProvider';
 
+const isValidUrl = (url: string) => {
+  const urlRegex = new RegExp("^" + // protocol identifier
+    "(?:(?:https?|ftp)://)?" + // ** mod: make scheme optional
+    // user:pass authentication
+    "(?:\\S+(?::\\S*)?@)?" + "(?:" + // IP address exclusion
+    // private & local networks
+    // "(?!(?:10|127)(?:\\.\\d{1,3}){3})" +   // ** mod: allow local networks
+    // "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" +  // ** mod: allow local networks
+    // "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +  // ** mod: allow local networks
+    // IP address dotted notation octets
+    // excludes loopback network 0.0.0.0
+    // excludes reserved space >= 224.0.0.0
+    // excludes network & broacast addresses
+    // (first & last IP address of each class)
+    "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" + "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" + "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" + "|" + // host name
+    "(?:(?:[a-zA-Z\\u00a1-\\uffff0-9]-*)*[a-zA-Z\\u00a1-\\uffff0-9]+)" + // domain name
+    "(?:\\.(?:[a-zA-Z\\u00a1-\\uffff0-9]-*)*[a-zA-Z\\u00a1-\\uffff0-9]+)*" + // TLD identifier
+    "(?:\\.(?:[a-zA-Z\\u00a1-\\uffff]{2,}))" + ")" + // port number
+    "(?::\\d{2,5})?" + // resource path
+    "(?:/\\S*)?" + "$");
+
+  return urlRegex.test(url);
+};
+
 type PriceWithChangeProps = {
   symbol: string;
 };
@@ -34,7 +58,7 @@ const TokenInfoItem: React.FC<TokenInfoItemProps> = (props) => {
   return (
     <ListItem bottomDivider onPress={onPressHandler}
       containerStyle={{ backgroundColor: COLORS.dark0, borderBottomColor: COLORS.dark4 }}>
-      {logoURI ? (
+      {logoURI && isValidUrl(logoURI) ? (
         <Avatar rounded source={{ uri: logoURI }} />
       ) : (
         <Avatar rounded title={symbol.toUpperCase()} />
