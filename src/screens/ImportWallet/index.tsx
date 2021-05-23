e10ca4bf56367f00ddc021ce14aa1f359c21c7e8
Tam Pham <tampham47@live.com>
2021-05-23 18:41:59 +0700

    Create import wallet screen

--- src/screens/ImportWallet/index.tsx
+++ src/screens/ImportWallet/index.tsx
@@ -1,22 +1,85 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { Button } from 'react-native-elements';
+import React, { useState } from 'react';
+import { View, Text, SafeAreaView, ScrollView, TextInput, StyleSheet } from 'react-native';
+import { Button, Input } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
+import Clipboard from '@react-native-community/clipboard';
 
 import Routes from '../../navigators/Routes';
+import { grid, typo, input } from '../../components/Styles';
+import { COLORS, FONT_SIZES, LINE_HEIGHT } from '../../theme';
+import WalletFactory from '../../factory/Wallet';
+
+const s = StyleSheet.create({
+  wrp: {
+    marginBottom: 12,
+  },
+  label: {
+    fontSize: FONT_SIZES.lg,
+    color: COLORS.white2,
+    marginBottom: 8,
+  },
+  recoverPhrase: {
+    backgroundColor: COLORS.dark0,
+    color: COLORS.white2,
+    fontSize: FONT_SIZES.lg,
+    lineHeight: LINE_HEIGHT.lg,
+    borderColor: COLORS.dark0,
+    borderRadius: 4,
+    borderWidth: 2,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    height: 200,
+    flexWrap: 'wrap',
+    marginBottom: 8,
+  }
+});
 
 type Props = {};
 const ImportWallet: React.FC<Props> = () => {
   const navigation = useNavigation();
+  const [recovery, setRecovery] = useState('');
+  const [walletName, setWalletName] = useState('');
 
-  const onPressHandler = React.useCallback(() => {
+  const importWallet = async () => {
+    // TODO: Handle the seed later
+    await WalletFactory.create('seed', recovery, walletName, true);
     navigation.navigate(Routes.Home, { screen: Routes.Wallet });
-  }, [navigation]);
+  };
+
+  const paste = async () => {
+    const text = await Clipboard.getString();
+    setRecovery(text);
+  }
 
   return (
-    <View>
-      <Text>{`ImportWallet`}</Text>
-      <Button title="Import" onPress={onPressHandler} />
+    <View style={grid.container}>
+      <SafeAreaView style={grid.wrp}>
+        <ScrollView>
+          <View style={grid.content}>
+            <View style={s.wrp}>
+              <Text style={s.label}>Mã khôi phục</Text>
+              <TextInput
+                style={s.recoverPhrase}
+                multiline={true}
+                value={recovery}
+                onChangeText={text => setRecovery(text)} />
+              <Text style={typo.helper}>Bạn có thể nhập Mã khôi phục từ các SPL wallet khác như: Sollet</Text>
+              <Button title="Dán mã khôi phục" type="clear" onPress={paste} />
+            </View>
+
+            <Input
+              label="Tên Ví"
+              placeholder=""
+              style={typo.input}
+              labelStyle={input.label}
+              containerStyle={input.container}
+              value={walletName}
+              onChangeText={text => setWalletName(text)}
+            />
+            <Button title="Import" onPress={importWallet} style={grid.button} />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     </View>
   );
 };
