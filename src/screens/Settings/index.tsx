3e562de7179aebe41c62c5c4e9a02dfdce938a56
Tam Pham <tampham47@live.com>
2021-07-27 04:31:40 +0700

    Update settings page

--- src/screens/Settings/index.tsx
+++ src/screens/Settings/index.tsx
@@ -1,59 +1,11 @@
 import React from 'react';
 import { ScrollView, View, Text, StyleSheet } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import { ListItem, Icon } from 'react-native-elements';
 
 import { COLORS } from '../../theme/colors';
-import Icon from '../../components/Icon';
 import { spacings } from '../../theme';
-import { groups } from './data';
-import { MenuGroupType, MenuItemType } from './types';
 
-const MenuItem: React.FC<MenuItemType> = ({ name, icon }) => {
-  const onPressHandler = React.useCallback(() => {
-    console.log('onPressHandler');
-  }, []);
-
-  return (
-    <ListItem
-      bottomDivider
-      onPress={onPressHandler}
-      containerStyle={styles.item}
-    >
-      <Icon name={icon.name} color={icon.color} size={20} />
-      <ListItem.Content>
-        <ListItem.Title style={styles.itemTitle}>{name}</ListItem.Title>
-      </ListItem.Content>
-      <ListItem.Chevron color="grey" />
-    </ListItem>
-  );
-};
-
-const MenuGroup: React.FC<MenuGroupType> = (props) => {
-  return (
-    <View style={styles.group}>
-      {props.name !== null ? (
-        <Text style={styles.groupName}>{props.name}</Text>
-      ) : null}
-      {props.items.map((item, index) => (
-        <MenuItem key={index} {...item} />
-      ))}
-    </View>
-  );
-};
-
-const Settings: React.FC = () => {
-  return (
-    <View style={styles.container}>
-      <ScrollView>
-        {groups.map((group, index) => {
-          return <MenuGroup key={index} {...group} />;
-        })}
-      </ScrollView>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
+const s = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.dark2,
@@ -75,5 +27,50 @@ const styles = StyleSheet.create({
     color: COLORS.white2,
   },
 });
+
+const Settings: React.FC = () => {
+  return (
+    <View style={s.container}>
+      <ScrollView>
+        <View style={s.group}>
+          <ListItem bottomDivider onPress={() => null} containerStyle={s.item}>
+            <Icon type="antdesign" name="Safety" color="grey" size={20} />
+            <ListItem.Content>
+              <ListItem.Title style={s.itemTitle}>Ví</ListItem.Title>
+            </ListItem.Content>
+            <ListItem.Chevron color="grey" />
+          </ListItem>
+        </View>
+
+        <View style={s.group}>
+          <Text style={s.groupName}>Cộng đồng</Text>
+          <ListItem bottomDivider onPress={() => null} containerStyle={s.item}>
+            <Icon type="antdesign" name="staro" color="grey" size={20} />
+            <ListItem.Content>
+              <ListItem.Title style={s.itemTitle}>WealthClub</ListItem.Title>
+            </ListItem.Content>
+            <ListItem.Chevron color="grey" />
+          </ListItem>
+
+          <ListItem bottomDivider onPress={() => null} containerStyle={s.item}>
+            <Icon type="feather" name="twitter" color="grey" size={20} />
+            <ListItem.Content>
+              <ListItem.Title style={s.itemTitle}>Twitter</ListItem.Title>
+            </ListItem.Content>
+            <ListItem.Chevron color="grey" />
+          </ListItem>
+
+          <ListItem bottomDivider onPress={() => null} containerStyle={s.item}>
+            <Icon type="antdesign" name="hearto" color="grey" size={20} />
+            <ListItem.Content>
+              <ListItem.Title style={s.itemTitle}>Telegram</ListItem.Title>
+            </ListItem.Content>
+            <ListItem.Chevron color="grey" />
+          </ListItem>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
 
 export default Settings;
