f57d8364fefaeb2ca4668778e34b4a98225caefe
Tam Pham <tampham47@live.com>
2021-08-07 15:30:28 +0700

    Show svg icons

--- src/components/TokensList/index.tsx
+++ src/components/TokensList/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 import { Avatar, ListItem } from 'react-native-elements';
 import { useNavigation } from '@react-navigation/native';
+import { SvgXml } from 'react-native-svg';
+import LoadingIndicator from '../LoadingIndicator';
 
 import { COLORS, FONT_SIZES } from '../../theme';
 import Routes from '../../navigators/Routes';
@@ -9,35 +11,65 @@ import { useApp } from '../../core/AppProvider';
 import { price } from '../../utils/autoRound';
 
 const isValidUrl = (url: string) => {
-  const urlRegex = new RegExp(
-    '^' + // protocol identifier
-    '(?:(?:https?|ftp)://)?' + // ** mod: make scheme optional
-    // user:pass authentication
-    '(?:\\S+(?::\\S*)?@)?' +
-    '(?:' + // IP address exclusion
-    // private & local networks
-    // "(?!(?:10|127)(?:\\.\\d{1,3}){3})" +   // ** mod: allow local networks
-    // "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" +  // ** mod: allow local networks
-    // "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +  // ** mod: allow local networks
-    // IP address dotted notation octets
-    // excludes loopback network 0.0.0.0
-    // excludes reserved space >= 224.0.0.0
-    // excludes network & broacast addresses
-    // (first & last IP address of each class)
-    '(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])' +
-    '(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}' +
-    '(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))' +
-    '|' + // host name
-    '(?:(?:[a-zA-Z\\u00a1-\\uffff0-9]-*)*[a-zA-Z\\u00a1-\\uffff0-9]+)' + // domain name
-    '(?:\\.(?:[a-zA-Z\\u00a1-\\uffff0-9]-*)*[a-zA-Z\\u00a1-\\uffff0-9]+)*' + // TLD identifier
-    '(?:\\.(?:[a-zA-Z\\u00a1-\\uffff]{2,}))' +
-    ')' + // port number
-    '(?::\\d{2,5})?' + // resource path
-    '(?:/\\S*)?' +
-    '$',
-  );
+  return url.indexOf('.png') >= 0 || url.indexOf('.svg') >= 0;
+};
 
-  return urlRegex.test(url);
+const iconStyle = StyleSheet.create({
+  main: {
+    width: 36,
+    height: 36,
+    borderRadius: 18,
+    overflow: 'hidden',
+  },
+});
+
+const getWidth = (svgString: string): number => {
+  const startIndex = svgString.indexOf('width="') + 7;
+  const endIndex = svgString.indexOf('"', startIndex);
+  const w = svgString.substr(startIndex, endIndex - startIndex);
+  return isNaN(parseInt(w)) ? 18 : parseInt(w);
+};
+
+const CryptoIcon = ({ uri, ...props }) => {
+  const [width, setWidth] = useState(18);
+  const [svgFile, setSvgFile] = useState('');
+  const [loading, setLoading] = useState(true);
+  const isSVG = uri.indexOf('.svg') >= 0;
+
+  useEffect(() => {
+    if (!isSVG) {
+      return;
+    }
+
+    fetch(uri, { method: 'GET' })
+      .then((res) => {
+        return res.text();
+      })
+      .then((svg) => {
+        const width = getWidth(svg);
+        setWidth(width);
+        setSvgFile(svg);
+        setLoading(false);
+      });
+  }, []);
+
+  if (isSVG) {
+    return (
+      <View style={iconStyle.main}>
+        {loading ? (
+          <LoadingIndicator />
+        ) : (
+          <SvgXml
+            width="100%"
+            height="100%"
+            viewBox={`0 0 ${width} ${width}`}
+            xml={svgFile}
+          />
+        )}
+      </View>
+    );
+  }
+  return <Avatar {...props} source={{ uri: uri }} />;
 };
 
 type TokenInfoItemProps = TokenInfo & {
@@ -71,7 +103,7 @@ const TokenInfoItem: React.FC<TokenInfoItemProps> = ({ action, ...props }) => {
       }}
     >
       {logoURI && isValidUrl(logoURI) ? (
-        <Avatar rounded source={{ uri: logoURI }} />
+        <CryptoIcon rounded uri={logoURI} />
       ) : (
         <Avatar rounded title={symbol.toUpperCase()} />
       )}
