476b3466547f71ef0049f3163b61d74d62bc7083
Tam Pham <tampham47@live.com>
2021-08-17 14:04:53 +0700

    show price at the first time

--- src/core/AppProvider/index.tsx
+++ src/core/AppProvider/index.tsx
@@ -115,10 +115,12 @@ export const AppProvider: React.FC = (props) => {
   };
 
   const loadAccountFromStore = async (owner: string) => {
+    const priceMapping = await fetchPriceData(tokenInfos);
     const list = await getAccountListByOwner(owner);
     const storeAccList = createAccountList(tokenInfos, list, priceData);
 
     setAccountListSource(storeAccList);
+    setPriceData(priceMapping);
   };
 
   const loadAccountList = async () => {
