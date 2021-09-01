7a8674cdc7cc62ecda0d2a7ddf2a7fc9119a098f
Tam Pham <tampham47@live.com>
2021-09-01 20:18:34 +0700

    support multiple accounts

--- src/core/AppProvider/IAccount.tsx
+++ src/core/AppProvider/IAccount.tsx
@@ -19,22 +19,24 @@ export const createAccountList = (
   accountList: any[],
   priceData = {},
 ): IAccount[] => {
-  return tokenList.map((i) => {
-    const id = i.extensions?.coingeckoId || '-';
-    const address = i.address ? i.address : '';
-    const account = accountList?.find((acc) => acc.mint === address) || {};
+  const accountIdList = accountList.map((i) => i.mint).join(',');
+
+  // filled token info for existing account list
+  const filledAccountList = [...accountList].map((account) => {
+    const token = tokenList.find((t) => t.address === account.mint) || {};
+    const id = token.extensions?.coingeckoId || '-';
     const price = priceData[id] || account;
     const usd = price.usd || 0;
     const amount = account.amount || 0;
     const decimals = account.decimals || 0;
     const value = (amount / Math.pow(10, decimals)) * usd;
-    const sortName = i.name
+    const sortName = token.name
       .replace('Wrapped', '')
       .replace('(Sollet)', '')
       .trim();
 
     return {
-      ...i,
+      ...token,
       mint: account.mint || '',
       publicKey: account.publicKey || '',
       owner: account.owner || '',
@@ -48,4 +50,38 @@ export const createAccountList = (
       sortName,
     } as IAccount;
   });
+
+  const filledTokenList = [...tokenList]
+    // filter non-created token
+    .filter((t) => accountIdList.indexOf(t.address) < 0)
+    .map((i) => {
+      const id = i.extensions?.coingeckoId || '-';
+      const account = {};
+      const price = priceData[id] || account;
+      const usd = price.usd || 0;
+      const amount = account.amount || 0;
+      const decimals = account.decimals || 0;
+      const value = (amount / Math.pow(10, decimals)) * usd;
+      const sortName = i.name
+        .replace('Wrapped', '')
+        .replace('(Sollet)', '')
+        .trim();
+
+      return {
+        ...i,
+        mint: account.mint || '',
+        publicKey: account.publicKey || '',
+        owner: account.owner || '',
+        amount,
+        decimals,
+        value,
+        valid: account.valid || false,
+        isMinted: !!account.publicKey,
+        usd: price.usd || 0,
+        vnd: price.vnd || 0,
+        sortName,
+      } as IAccount;
+    });
+
+  return [...filledAccountList, ...filledTokenList];
 };
