791c10ed817df2dd047957ea344318c4bdb09b0c
Tam Pham <tampham47@live.com>
2021-09-18 17:57:45 +0700

    Showing token list in correct way

--- src/components/TokensList/index.tsx
+++ src/components/TokensList/index.tsx
@@ -76,8 +76,12 @@ type TokensListProps = {
 const TokensList: React.FC<TokensListProps> = ({ balanceListInfo, action }) => {
   return (
     <>
-      {balanceListInfo?.map((token) => (
-        <TokenInfoItem key={token.publicKey} token={token} action={action} />
+      {balanceListInfo?.map((token, index) => (
+        <TokenInfoItem
+          key={`${index}-${token.publicKey}`}
+          token={token}
+          action={action}
+        />
       ))}
     </>
   );
