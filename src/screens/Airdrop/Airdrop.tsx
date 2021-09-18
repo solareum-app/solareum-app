14ba60ec70e32a55446219a3e122ea4348105915
Tam Pham <tampham47@live.com>
2021-09-18 15:34:40 +0700

    complete airdrop flow

--- src/screens/Airdrop/Airdrop.tsx
+++ src/screens/Airdrop/Airdrop.tsx
@@ -1,8 +1,9 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 import { Portal } from 'react-native-portalize';
 import { Button } from 'react-native-elements';
 
+import { useToken } from '../../core/AppProvider/TokenProvider';
 import { FixedContent } from '../../components/Modals/FixedContent';
 import { COLORS } from '../../theme';
 import { typo } from '../../components/Styles';
@@ -12,6 +13,8 @@ import { AirdropStepInputRefAddress } from './AirdropStepInputRefAddress';
 import { AirdropStepReview } from './AirdropStepReview';
 import { AirdropStepSuccessAndShare } from './AirdropStepSuccessAndShare';
 import ImgPayment from '../../assets/clip-payment.png';
+import { authFetch } from '../../utils/authfetch';
+import { service } from '../../config';
 
 const s = StyleSheet.create({
   main: {
@@ -40,14 +43,78 @@ enum AIRDROP_STEP {
 }
 
 export const Airdrop = () => {
-  const [airdrop, setAirdrop] = useState(true);
+  const { accountList } = useToken();
+  const [airdrop, setAirdrop] = useState(false);
+  const [rewardRef, setRewardRef] = useState(0);
   const [step, setStep] = useState(AIRDROP_STEP.info);
   const [refAddress, setRefAddress] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string>('');
+
+  const [airdropSignature, setAirdropSignature] = useState<string>('');
+  const [rewardRefSignature, setRewardRefSignature] = useState<string>('');
   const refStepInfo = useRef();
 
   const dismiss = () => {
     setAirdrop(false);
   };
+
+  const startAirdrop = () => {
+    setError('');
+    setRefAddress('');
+    setStep(AIRDROP_STEP.info);
+    refStepInfo.current?.open();
+  };
+
+  const submit = async () => {
+    setLoading(true);
+    const solAccount = accountList.find((i) => i.mint === 'SOL');
+    const xsbAccount = accountList.find((i) => i.symbol === 'XSB');
+
+    if (!solAccount?.publicKey || !xsbAccount?.publicKey) {
+      setError(
+        'Tài khoản XSB chưa được khởi tạo, bạn chưa thể hoàn thành airdrop.',
+      );
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const resp = await authFetch(service.postAirdrop, {
+        method: 'POST',
+        body: {
+          solAddress: solAccount?.publicKey,
+          xsbAddress: xsbAccount?.publicKey,
+          refAddress,
+        },
+      });
+      setAirdrop(resp.rewardAirdrop);
+      setRewardRef(resp.rewardRef);
+      setAirdropSignature(resp.rewardAirdropSignature);
+      setRewardRefSignature(resp.rewardRefSignature);
+    } catch {
+      setError('Có lỗi xảy ra, vui lòng thử lại sau.');
+    } finally {
+      setStep(AIRDROP_STEP.successAndShare);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      const solAccount = accountList.find((i) => i.mint === 'SOL');
+      const xsbAccount = accountList.find((i) => i.symbol === 'XSB');
+      const t = await authFetch(service.postCheckAirdrop, {
+        method: 'POST',
+        body: {
+          solAddress: solAccount?.publicKey,
+          xsbAddress: xsbAccount?.publicKey,
+        },
+      });
+      setAirdrop(t.rewardAirdrop);
+      setRewardRef(t.rewardRef);
+    })();
+  }, []);
 
   if (!airdrop) {
     return null;
@@ -62,14 +129,7 @@ export const Airdrop = () => {
           XSB là token sẽ được sử dụng trong Solareum Lightning, một ứng dụng
           web3.0 giúp thưởng cho những nhà phát triển nội dung kỹ thuật số.
         </Text>
-        <Button
-          title="Nhận XSB"
-          type="outline"
-          onPress={() => {
-            setStep(AIRDROP_STEP.info);
-            refStepInfo.current?.open();
-          }}
-        />
+        <Button title="Nhận +20 XSB" type="outline" onPress={startAirdrop} />
       </View>
 
       <Portal>
@@ -97,13 +157,23 @@ export const Airdrop = () => {
 
           {step === AIRDROP_STEP.review ? (
             <AirdropStepReview
-              next={() => setStep(AIRDROP_STEP.successAndShare)}
+              next={submit}
+              airdrop={airdrop}
+              rewardRef={rewardRef}
               refAddress={refAddress}
+              loading={loading}
+              error={error}
             />
           ) : null}
 
           {step === AIRDROP_STEP.successAndShare ? (
-            <AirdropStepSuccessAndShare next={dismiss} />
+            <AirdropStepSuccessAndShare
+              next={dismiss}
+              airdrop={airdrop}
+              rewardRef={rewardRef}
+              airdropSignature={airdropSignature}
+              rewardRefSignature={rewardRefSignature}
+            />
           ) : null}
         </FixedContent>
       </Portal>
--- src/screens/Airdrop/AirdropStepSuccessAndShare.tsx
+++ src/screens/Airdrop/AirdropStepSuccessAndShare.tsx
@@ -1,50 +1,126 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Share, Linking } from 'react-native';
 import { Button } from 'react-native-elements';
+import LottieView from 'lottie-react-native';
 
 import { grid, typo } from '../../components/Styles';
 import { useToken } from '../../core/AppProvider/TokenProvider';
-import { SOL_BALANCE_TARGET } from './const';
 
 import { style as s } from './style';
+import { COLORS } from '../../theme';
 
-export const AirdropStepSuccessAndShare = ({ next }) => {
+const style = StyleSheet.create({
+  imgWrp: {
+    width: 120,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  img: {
+    width: 120,
+    height: 120,
+  },
+
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 40,
+    marginBottom: 20,
+    marginTop: -10,
+  },
+  rowItem: {
+    flex: 1,
+    // backgroundColor: 'blue',
+  },
+  rowItemRight: {
+    flex: 0,
+  },
+  value: {
+    lineHeight: 40,
+    color: COLORS.white2,
+    fontSize: 20,
+  },
+});
+
+export const AirdropStepSuccessAndShare = ({
+  next,
+  airdrop,
+  rewardRef,
+  airdropSignature,
+  rewardRefSignature,
+}) => {
   const { accountList } = useToken();
-
   const solAccount = accountList.find((i) => i.mint === 'SOL');
-  let active = false;
-  if (solAccount) {
-    active =
-      solAccount.amount * Math.pow(10, solAccount.decimals) >=
-      SOL_BALANCE_TARGET;
-  }
+
+  const onShare = async () => {
+    try {
+      const message = `Tải Solareum Wallet - https://solareum.app/getwallet. Nhập địa chỉ SOL của mình để nhận Airdrop nhé - ${solAccount?.publicKey}`;
+
+      const result = await Share.share({
+        message,
+      });
+      return result;
+    } catch { }
+  };
 
   return (
     <View style={s.main}>
       <Text style={typo.title}>Nhận Airdrop thành công</Text>
-      <Text style={typo.normal}>Cảm ơn bạn đã tham gia chương trình.</Text>
+      <View style={style.imgWrp}>
+        <LottieView
+          autoPlay
+          loop
+          source={require('../../theme/lottie/award-badge.json')}
+          style={style.img}
+        />
+      </View>
       <View style={grid.group}>
         <Text style={grid.groupTitle}>Bạn nhận được</Text>
-        <Text style={grid.groupValue}>+20 XSB</Text>
+        <View style={style.row}>
+          <View style={style.rowItem}>
+            <Text style={style.value}>+{airdrop} XSB</Text>
+          </View>
+          <View style={style.rowItemRight}>
+            <Button
+              title="scan"
+              type="clear"
+              onPress={() => {
+                Linking.openURL(`https://solscan.io/tx/${airdropSignature}`);
+              }}
+            />
+          </View>
+        </View>
+
         <Text style={grid.groupTitle}>Người giới thiệu nhận được</Text>
-        <Text style={grid.groupValue}>+75 XSB</Text>
+        <View style={style.row}>
+          <View style={style.rowItem}>
+            <Text style={style.value}>+{rewardRef} XSB</Text>
+          </View>
+          <View style={style.rowItemRight}>
+            <Button
+              title="scan"
+              type="clear"
+              onPress={() => {
+                Linking.openURL(`https://solscan.io/tx/${rewardRefSignature}`);
+              }}
+            />
+          </View>
+        </View>
       </View>
+      <Text style={typo.normal}>Cảm ơn bạn đã tham gia chương trình,</Text>
       <Text style={typo.normal}>
         Hãy trở thành người giới thiệu bằng cách chia sẻ app Solareum tới bạn bè
-        và người thân, để nhận được phần thưởng không giới hạn từ&nbsp;Solareum.
+        và người thân, để nhận được XSB không giới hạn từ&nbsp;Solareum.
       </Text>
       <View style={s.footer}>
         <Button
           type="outline"
           title="Chia sẻ Solareum Wallet"
-          disabled={!active}
-          onPress={next}
-        />
-        <Button
-          type="clear"
-          title="Thông tin chi tiết"
-          containerStyle={s.button}
-          onPress={next}
+          onPress={() => {
+            onShare();
+            next();
+          }}
         />
       </View>
     </View>
