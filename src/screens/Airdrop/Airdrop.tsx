9131c407e18bd7394d76fd27d67184472a20a21f
Tam Pham <tampham47@live.com>
2021-09-25 17:32:54 +0700

    Sending check airdrop request after sol account is init

--- src/screens/Airdrop/Airdrop.tsx
+++ src/screens/Airdrop/Airdrop.tsx
@@ -113,6 +113,10 @@ export const Airdrop = ({ isActive }) => {
     (async () => {
       const solAccount = accountList.find((i) => i.mint === 'SOL');
       const xsbAccount = accountList.find((i) => i.symbol === 'XSB');
+      if (!solAccount) {
+        return;
+      }
+
       const resp = await authFetch(service.postCheckAirdrop, {
         method: 'POST',
         body: {
@@ -123,10 +127,11 @@ export const Airdrop = ({ isActive }) => {
           },
         },
       });
-      setAirdrop(parseFloat(resp.rewardAirdrop));
-      setRewardRef(parseFloat(resp.rewardRef));
+
+      setAirdrop(resp.rewardAirdrop);
+      setRewardRef(resp.rewardRef);
     })();
-  }, []);
+  }, [accountList]);
 
   if (!airdrop && !isActive) {
     return null;
