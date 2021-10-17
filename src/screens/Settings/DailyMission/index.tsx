89567dd0fbc0ede3350941cb8252271919f63793
Tam Pham <tampham47@live.com>
2021-10-17 19:41:10 +0700

    Show mission reward in setting page

--- src/screens/Settings/DailyMission/index.tsx
+++ src/screens/Settings/DailyMission/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, Text, ScrollView, SafeAreaView } from 'react-native';
 
 import { Banner } from '../../../components/Admob/Rewarded';
@@ -6,6 +6,11 @@ import { grid, typo } from '../../../components/Styles';
 import { useConfig } from '../../../core/AppProvider/RemoteConfigProvider';
 import { useLocalize } from '../../../core/AppProvider/LocalizeProvider';
 import { MissionButton } from '../../../containers/MissionButton';
+
+import { authFetch } from '../../../utils/authfetch';
+import { service } from '../../../config';
+import { useToken } from '../../../core/AppProvider/TokenProvider';
+import { useMetaData } from '../../../hooks/useMetaData';
 
 const s = StyleSheet.create({
   wrp: {
@@ -20,8 +25,30 @@ const s = StyleSheet.create({
 type Props = {};
 
 const DailyMission: React.FC<Props> = () => {
+  const [missionReward, setMissionReward] = useState(0);
+  const metaData = useMetaData();
+  const { accountList } = useToken();
   const { admob } = useConfig();
   const { t } = useLocalize();
+
+  const solAccount = accountList.find((i) => i.mint === 'SOL');
+
+  const loadCheckMission = async () => {
+    const resp = await authFetch(service.postCheckMission, {
+      method: 'POST',
+      body: {
+        solAddress: solAccount?.publicKey,
+        meta: {
+          ...metaData,
+        },
+      },
+    });
+    setMissionReward(resp.missionReward);
+  };
+
+  useEffect(() => {
+    loadCheckMission();
+  }, [accountList]);
 
   return (
     <View style={grid.container}>
@@ -33,7 +60,7 @@ const DailyMission: React.FC<Props> = () => {
               <Text style={typo.normal}>{t('airdrop-mission-message-02')}</Text>
               <Text style={typo.normal}>{t('airdrop-mission-message-03')}</Text>
               <Text style={typo.normal}>
-                {t('airdrop-mission-message-04', { amount: 0 })}
+                {t('airdrop-mission-message-04', { amount: missionReward })}
               </Text>
             </View>
 
