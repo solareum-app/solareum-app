aaf3134fcd0a1ae649b6bd95fc0517fd564ebc2c
Tam Pham <tampham47@live.com>
2021-10-29 08:39:41 +0700

    Update reward flow

--- src/screens/Settings/DailyMission/index.tsx
+++ src/screens/Settings/DailyMission/index.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, Text, ScrollView, SafeAreaView } from 'react-native';
 
-import { Banner } from '../../../components/Admob/Rewarded';
 import { grid, typo } from '../../../components/Styles';
 import { useConfig } from '../../../core/AppProvider/RemoteConfigProvider';
 import { useLocalize } from '../../../core/AppProvider/LocalizeProvider';
@@ -56,28 +55,22 @@ const DailyMission: React.FC<Props> = () => {
         <ScrollView>
           <View style={grid.content}>
             <View style={s.wrp}>
+              <Text style={typo.normal}>{t('airdrop-mission-message-01')}</Text>
+              <Text style={typo.normal}>{t('airdrop-mission-message-02')}</Text>
+              <Text style={typo.normal}>
+                {t('airdrop-mission-message-03')}{' '}
+                {t('airdrop-mission-message-04', { amount: missionReward })}
+              </Text>
+              <Text style={typo.normal}>{t('airdrop-mission-message-05')}</Text>
+            </View>
+
+            <View style={s.wrp}>
               <MissionButton padding={0} />
               {!admob ? (
                 <Text style={s.helper}>
                   {t('airdrop-mission-lock-message')}
                 </Text>
               ) : null}
-            </View>
-
-            <View style={s.wrp}>
-              <Text style={typo.normal}>{t('airdrop-mission-message-01')}</Text>
-            </View>
-
-            <View style={s.wrp}>
-              <Banner />
-            </View>
-
-            <View>
-              <Text style={typo.normal}>{t('airdrop-mission-message-02')}</Text>
-              <Text style={typo.normal}>
-                {t('airdrop-mission-message-03')}{' '}
-                {t('airdrop-mission-message-04', { amount: missionReward })}
-              </Text>
             </View>
           </View>
         </ScrollView>
