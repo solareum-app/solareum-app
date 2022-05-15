5bc0536d2b20ad54aa1443808503e472d1e18e2a
Tam Pham <tampham47@live.com>
2022-05-15 09:42:25 +0700

    chore: update your referrals section

--- src/screens/Distribution/Mission.tsx
+++ src/screens/Distribution/Mission.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Share } from 'react-native';
 import { Button as SimpleBtn } from 'react-native-elements';
 
@@ -8,12 +8,20 @@ import { Button } from '../../components/Button/Button';
 import { usePrice } from '../../core/AppProvider/PriceProvider';
 import { useLocalize } from '../../core/AppProvider/LocalizeProvider';
 import { COLORS } from '../../theme';
+import { box } from './style';
+import { authFetch } from '../../utils/authfetch';
 
 const s = StyleSheet.create({
   main: {
     marginTop: 24,
     marginBottom: 40,
     padding: 0,
+  },
+  mission: {
+    marginBottom: 48,
+  },
+  section: {
+    marginBottom: 24,
   },
   body: {
     marginBottom: 24,
@@ -28,7 +36,8 @@ const btn = StyleSheet.create({
     color: COLORS.blue,
   },
   body: {
-    marginTop: 12,
+    borderColor: COLORS.blue,
+    height: 56,
   },
 });
 
@@ -36,7 +45,11 @@ export const Mission = () => {
   const { t } = useLocalize();
   const { accountList } = usePrice();
   const { getRewards, loading, missionLeft } = useRewards();
+
+  const [noRef, setNoRef] = useState<number>(0);
+
   const solAccount = accountList.find((i) => i.mint === 'SOL');
+  const solAddress = solAccount?.publicKey;
 
   const onShare = async () => {
     try {
@@ -51,37 +64,68 @@ export const Mission = () => {
     } catch {}
   };
 
+  useEffect(() => {
+    (async () => {
+      const numberRef = await authFetch(
+        `/wallets/count?nominated_by=${solAddress}`,
+        {
+          method: 'get',
+        },
+      );
+      setNoRef(numberRef);
+    })();
+  }, [accountList]);
+
   return (
     <View>
       <View style={s.main}>
-        <View style={s.body}>
-          <Text style={typo.titleLeft}>
-            Complete the mission to get rewards
-          </Text>
-          <Text style={s.message}>
-            Every day you need to show up here to receive a distribution of
-            0.055% from your XSB balance.
-          </Text>
-          <Text style={s.message}>
-            Your referrer will receive 0.015% as well. Let's share Solareum with
-            your friends to receive unlimited referral fees.
-          </Text>
+        <View style={s.mission}>
+          <View style={s.body}>
+            <Text style={typo.titleLeft}>
+              Complete the mission to get rewards
+            </Text>
+            <Text style={s.message}>
+              Every day you need to show up here to receive a distribution of
+              0.055% from your XSB balance. Your referrer will receive 0.015% as
+              well.
+            </Text>
+          </View>
+
+          <Button
+            title={missionLeft > 0 ? 'Get Daily Rewards' : 'Mission Completed'}
+            onPress={getRewards}
+            disabled={missionLeft <= 0}
+            loading={loading}
+          />
         </View>
 
-        <Button
-          title={missionLeft > 0 ? 'Get Daily Rewards' : 'Mission Completed'}
-          onPress={getRewards}
-          disabled={missionLeft <= 0}
-          loading={loading}
-        />
+        <View style={s.section}>
+          <View style={s.body}>
+            <Text style={typo.titleLeft}>Your Referrals</Text>
+            <Text style={s.message}>
+              Let's share Solareum with your friends to receive unlimited
+              referral fees.
+            </Text>
 
-        <SimpleBtn
-          type="clear"
-          title="Share Solareum"
-          onPress={onShare}
-          titleStyle={btn.title}
-          buttonStyle={btn.body}
-        />
+            <View style={box.main}>
+              <View style={box.left}>
+                <Text style={box.title}>Total member</Text>
+              </View>
+              <View style={box.right}>
+                <View style={box.badge}>
+                  <Text style={box.value}>{noRef}</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+          <SimpleBtn
+            type="outline"
+            title="Share Solareum"
+            onPress={onShare}
+            titleStyle={btn.title}
+            buttonStyle={btn.body}
+          />
+        </View>
       </View>
     </View>
   );
