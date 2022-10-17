c8734d3d82e1ed8070a51ad647a6f33d21e99e88
Tam Pham <tampham47@live.com>
2022-10-17 10:16:04 +0700

    chore: update api

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const MAINNET_URL = 'http://api.mainnet-beta.solana.com';
+export const MAINNET_URL = 'https://api.mainnet-beta.solana.com';
 // export const MAINNET_URL = 'https://solana-api.projectserum.com';
 
 // DEV
